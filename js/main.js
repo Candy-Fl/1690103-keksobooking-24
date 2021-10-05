435a9bfc1c7867aefe65d0119982d8a59a9f4af3
candyy <alekseykim01@gmail.com>
2021-10-06 00:02:38 +0700

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,22 +1,186 @@
 // Функция, которая выводит случайное число в диапозоне от min до max
-function selfRandom(min, max){
-  if (max>min) {
-    return Math.floor((Math.random() * (max - min + 1)) + min);
-  }
-  throw new Error ('min должно быть больше max');
-}
-selfRandom(1,20);
+function getSelfRandom(a, b){
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 
-function coordinateRandom(min, max, number){
-  if ((max>min) && (min>0) && (max>0)) {
-    const random = Math.random() * (max - min + 1) + min; //Генерация рандомного числа в диапозоне (число дробное)
-    const remainder = 10**number; // Получения числа, для определения количества знаков
-    return (Math.ceil(random*remainder)/remainder);//Вычисление нужного числа
-  }
-  throw new Error('Проверьте введенные числа!');
+function getRandomPositiveFloat (a, b, digits ) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+  return parseFloat(result.toFixed(digits),10);
 }
 
-coordinateRandom(1,20,4);
+// Массив с номерами для адреса изображения автора
+const AVATAR_INDEX = [
+  '01',
+  '02',
+  '03',
+  '04',
+  '05',
+  '06',
+  '07',
+  '08',
+  '09',
+  '10'
+];
 
+// Заголовок
+
+const TITLE = [
+  'К вашему вниманию, лучшее предложение, что вы видели на данный момент!'
+];
+
+// Получение рандомных координат
+
+const getRandomLat = () => {
+  return (getRandomPositiveFloat(35.65000,35.70000,3));
+}
+
+const getRandomLng = () => {
+  return (getRandomPositiveFloat(139.70000,139.80000,4));
+}
+
+// Получаем случайную стоимость
+
+const PRICE = [
+  getSelfRandom(1,100000)
+];
+
+// Получаем тип жилья
+
+const TYPE = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+// Получаем число комнат
+
+const ROOMS = [
+  getSelfRandom(1,100000),
+];
+
+// Получаем число гостей
+
+const GUESTS = [
+  getSelfRandom(1,100000),
+];
+
+// Получаем дату заеда
+
+const CHECKIN = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+// Получаем дату выезда
+
+const CHECKOUT = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+// Массив приемуеств
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+// Описание квартиры
+
+const DESCRIPTION = [
+  'Как и говорилось ранее, квартира шикарна!'
+];
+
+// Фото квартиры
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+// Получаем случайный элемент массива
+
+const getRandomArrayElement = (elements) => {
+  return(elements[getSelfRandom(0,elements.length-1)])
+};
+
+// Получаем несколько случайных элементов массива, которые не будут повторяться
+
+const getRandomCount = (elements) => {
+  newElements = [];
+  copyElements = elements.slice(0);
+  countRandom = getSelfRandom(1,elements.length-1);
+  for (let i = 1 ; i<countRandom ; i++) {
+    randomIndex = getSelfRandom(0,elements.length-i);
+    newElements.push(copyElements[randomIndex]);
+    copyElements.splice(randomIndex,1);
+  }
+  return newElements;
+};
+
+// Создаём объект , который описывает автора
+
+const createAuthor = () => {
+  return {
+    avatar: 'img/avatars/user' + getRandomArrayElement(AVATAR_INDEX) + '.png',
+  }
+};
+
+// Создаём объект , который описывает информацию об объявлении
+
+const createOffer = () => {
+  return {
+    author : {
+      avatar: 'img/avatars/user' + getRandomArrayElement(AVATAR_INDEX) + '.png',
+    },
+    location : {
+      lat : getRandomLat(),
+      lng : getRandomLng(),
+    },
+    offer : {
+      title : getRandomArrayElement(TITLE),
+      address: [
+        String(location.lat)+' '+String(location.lng)
+      ],
+      price : getRandomArrayElement(PRICE),
+      type: getRandomArrayElement(TYPE),
+      rooms : getRandomArrayElement(ROOMS),
+      guests : getRandomArrayElement(GUESTS),
+      checkin : getRandomArrayElement(CHECKIN),
+      checkout : getRandomArrayElement(CHECKOUT),
+      features : getRandomCount(FEATURES),
+      description: getRandomArrayElement(DESCRIPTION),
+      photos : getRandomArrayElement(PHOTOS),
+    }
+  }
+}
+
+
+// Создаём один объект
+
+const getSameOffer = () => {
+  return {
+    author : createAuthor(),
+    offer : createOffer(),
+    location : LOCATION
+  }
+};
+
+// Создаём массив из 10 объектов
+const sameOfferList=Array.from({length:10},getSameOffer);
