e9e4d62219f3d6cf40e2fa62543e7f892aea4d6a
candyy <alekseykim01@gmail.com>
2021-11-02 01:06:32 +0700

    Отрисуй меня полностью (часть 2).2

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ import { sameOfferList } from './create-offer-list.js';
 import './data.js';
 import './generation-form.js';
 import './util.js';
-import {generateForm} from './generation-form.js';
+// import {generateForm} from './generation-form.js';
 import {disableForm} from './disable-form.js';
 disableForm();
 // eslint-disable-next-line no-console
