7cce47e6de2ced16beeec41c8b7225fcb902d6ce
candyy <alekseykim01@gmail.com>
2021-11-15 17:08:08 +0700

    Помощь друга

---
+++ js/toggle-form.js
@@ -0,0 +1,40 @@
+const adForm = document.querySelector('.ad-form');
+const mapFilters = document.querySelector('.map__filters');
+const disableForm = () => {
+  // Отключаем все элементы в информации об объявлении
+  adForm.classList.add('ad-form--disabled');
+  const adFormElements=  adForm.querySelectorAll('.ad-form__element');
+  (adFormElements).forEach((element) => {
+    element.classList.add('.disabled');
+  });
+  // Так же фильтры
+  mapFilters.classList.add('map__filters--disabled');
+  const mapFiltersContainer = mapFilters.querySelectorAll('.map__filter');
+  (mapFiltersContainer).forEach((element) => {
+    element.classList.add('.disabled');
+  });
+  // Отключаем интерактивные элементы приемуществ
+  mapFilters.querySelector('.map__features').classList.add('disabled');
+};
+
+export{disableForm};
+
+// Переводим форму в активное состояние
+
+const activeForm = () => {
+  adForm.classList.remove('ad-form--disabled');
+  const adFormElements=  adForm.querySelectorAll('.ad-form__element');
+  (adFormElements).forEach((element) => {
+    element.classList.remove('.disabled');
+  });
+  mapFilters.classList.remove('map__filters--disabled');
+  const mapFiltersContainer = mapFilters.querySelectorAll('.map__filter');
+  (mapFiltersContainer).forEach((element) => {
+    element.classList.remove('.disabled');
+  });
+  mapFilters.querySelector('.map__features').classList.remove('disabled');
+};
+
+export{activeForm};
+
+
